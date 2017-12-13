b05f7385aa76aab49e507bf8183034f9f5abbdcb
trevortds <trevortds3@gmail.com>
2017-12-13 08:39:45 -0500

    changed xk to work with my setup, copied some stuff to trevor

--- keyboards/lets_split/keymaps/trevor/config.h
+++ keyboards/lets_split/keymaps/trevor/config.h
@@ -8,6 +8,14 @@ This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef CONFIG_USER_H
@@ -15,13 +23,29 @@ the Free Software Foundation, either version 2 of the License, or
 
 #include "../../config.h"
 
+/* Use I2C or Serial, not both */
+
+#define USE_SERIAL
+// #define USE_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+/* RGB stuff */
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 10
+
+/* stuff copied from xk */
+
 #define TAPPING_TOGGLE 1
 #define TAPPING_TERM 300
 #define ONESHOT_TAP_TOGGLE 2
 #define ONESHOT_LAYER_TOGGLE 2
 #define ONESHOT_TIMEOUT 800
-
-#define USE_I2C
 
 #define MOUSEKEY_INTERVAL 50
 #define MOUSEKEY_DELAY 0
@@ -32,19 +56,7 @@ the Free Software Foundation, either version 2 of the License, or
 #define MOUSEKEY_WHEEL_MAX_SPEED   8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
 
-<<<<<<< HEAD
-// #define MASTER_LEFT
-// #define MASTER_RIGHT
-#define EE_HANDS
-=======
-#define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
->>>>>>> initial commit
-
-
 #define PREVENT_STUCK_MODIFIERS
-#define IGNORE_MOD_TAP_INTERRUPT
-#define PERMISSIVE_HOLD
+
 
 #endif
--- keyboards/lets_split/keymaps/trevor/rules.mk
+++ keyboards/lets_split/keymaps/trevor/rules.mk
@@ -1,11 +1,7 @@
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 NKRO_ENABLE = no           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-<<<<<<< HEAD
-USE_I2C = yes
-=======
 USE_I2C = no
->>>>>>> initial commit
 TAP_DANCE_ENABLE = yes
 
 CONSOLE_ENABLE = no         # Console for debug(+400)
@@ -15,12 +11,9 @@ MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = yes         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-<<<<<<< HEAD
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
-=======
 RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
->>>>>>> initial commit
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
+
 
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
