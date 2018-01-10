f859506e51d3ba8f27e831a0924b18d24158d644
trevortds <trevortds3@gmail.com>
2018-01-10 12:01:40 -0500

    fix osm

--- keyboards/ergodox_infinity/keymaps/trevor/keymap.c
+++ keyboards/ergodox_infinity/keymaps/trevor/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,               KC_1,    KC_2,    KC_3,    KC_4,  KC_5, KC_ESC,
   KC_TAB,               KC_Q,    KC_W,    KC_E,    KC_R,  KC_T, KC_LBRACKET,
   LT(_NAV,KC_BSPACE),   KC_A,    KC_S,    KC_D,    KC_F,  KC_G,
-  OSM(KC_LSFT),         KC_Z,    KC_X,    KC_C,    KC_V,  KC_B, KC_LPRN,
+  OSM(MOD_LSFT),         KC_Z,    KC_X,    KC_C,    KC_V,  KC_B, KC_LPRN,
   KC_LCTL,              KC_LGUI, KC_LGUI, KC_LALT, LOWER,
 
                                                   KC_MPLY,   TG(_MOUSE),
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TG(_NUM),     KC_6, KC_7,  KC_8,    KC_9,    KC_0,                KC_EQUAL,
   KC_RBRACKET, KC_Y, KC_U,  KC_I,    KC_O,    KC_P,                KC_BSLS,
                KC_H, KC_J,  KC_K,    KC_L,    KC_SCOLON,           LT(_NAV, KC_QUOTE),
-  KC_RPRN,      KC_N, KC_M,  KC_COMM, KC_DOT,  LT(_MOUSE, KC_SLSH), OSM(KC_RSFT),
+  KC_RPRN,      KC_N, KC_M,  KC_COMM, KC_DOT,  LT(_MOUSE, KC_SLSH), OSM(MOD_RSFT),
                      RAISE, KC_LEFT, KC_DOWN, KC_UP,               KC_RIGHT,
 
   TG(_NUM), TG(_MOUSE),
