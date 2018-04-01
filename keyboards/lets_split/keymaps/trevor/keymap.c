f9c7519b1b862eba3a1c4129854f45ef0a8ffec1
trevortds <trevortds3@gmail.com>
2018-04-01 09:50:12 -0400

    added modspc

--- keyboards/lets_split/keymaps/trevor/keymap.c
+++ keyboards/lets_split/keymaps/trevor/keymap.c
@@ -37,6 +37,7 @@ enum custom_keycodes {
 // navigation access keys
 #define BAKNAV LT(_NAV, KC_BSPC)
 #define QUONAV LT(_NAV, KC_QUOT)
+#define SUPSPC MT(MOD_LGUI, KC_SPC)
 
 // navigation layer keys
 #define W1 LGUI(KC_1)
@@ -70,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   BAKNAV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, QUONAV, \
   KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC , \
-  KC_LCTRL,KC_LGUI,  KC_LALT, ADJUST,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_LCTRL,KC_LGUI,  KC_LALT, ADJUST,  LOWER,   SUPSPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Colemak
