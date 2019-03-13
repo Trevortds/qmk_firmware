812c5b5d372a50a60a6bb0eeb4d23d46415fa32e
trevortds <trevortds3@gmail.com>
2019-03-13 10:57:06 -0400

    adding more details to flashletssplit

--- flashletssplit.sh
+++ flashletssplit.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
 make lets_split:trevor
+
 cp .build/lets_split_rev2_trevor.hex keyboards/lets_split/keymaps/trevor/
+
+echo "reset keyboard now!"
+sleep 5
+
 avrdude -p atmega32u4 -P /dev/ttyACM0 -c avr109 -U flash:w:lets_split_rev2_trevor.hex
+
+
